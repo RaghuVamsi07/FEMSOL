1de7ff7c17da493310106825063d21210e3837cd
RaghuVamsi07 <155610623+RaghuVamsi07@users.noreply.github.com>
2024-08-07 15:04:28 +0530

    Update app.py

--- GLML1DS/1D_Bars/app.py
+++ GLML1DS/1D_Bars/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__, static_folder='static')
 db_config = {
     'user': 'femsol_user',
     'password': 'Dreamsneverdie@21',
-    'host': 'your-rds-endpoint',  # Replace with your RDS endpoint
+    'host': 'database-1.c1c8ug44ytox.eu-north-1.rds.amazonaws.com',  # Replace with your RDS endpoint
     'database': 'femsol_db'
 }
 
