a416a3cd7e2f7f335de5ff3bbfb9162deea3bd6c
RaghuVamsi07 <155610623+RaghuVamsi07@users.noreply.github.com>
2024-08-10 14:58:25 +0530

    Update app.py

--- GLML1DS/1D_Bars/app.py
+++ GLML1DS/1D_Bars/app.py
@@ -42,14 +42,16 @@ def index():
         session['id'] = session_id
         return render_template('1D_Bars.html')
 
-@app.route('/add-line', methods=['POST'])
-def add_line():
+@app.route('/add-line-with-number', methods=['POST'])
+def add_line_with_number():
     data = request.json
-    session_id = request.cookies.get('session_id')
     conn = get_db_connection()
     cursor = conn.cursor()
-    query = "INSERT INTO lines_table (x1, y1, x2, y2, session_id) VALUES (%s, %s, %s, %s, %s)"
-    values = (data['x1'], data['y1'], data['x2'], data['y2'], session_id)
+    query = """
+    INSERT INTO lines_table (x1, y1, x2, y2, session_id, line_num)
+    VALUES (%s, %s, %s, %s, %s, %s)
+    """
+    values = (data['x1'], data['y1'], data['x2'], data['y2'], data['session_id'], data['line_num'])
     cursor.execute(query, values)
     conn.commit()
     line_id = cursor.lastrowid
