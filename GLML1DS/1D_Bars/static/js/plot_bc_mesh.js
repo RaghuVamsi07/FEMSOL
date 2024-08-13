1a94b1773d56bc4919657b7d2f9f85c1157d8a4c
RaghuVamsi07 <155610623+RaghuVamsi07@users.noreply.github.com>
2024-08-14 00:12:46 +0530

    Add files via upload

---
+++ GLML1DS/1D_Bars/static/js/plot_bc_mesh.js
@@ -0,0 +1,34 @@
+function plotPrimaryNodes(primaryNodes) {
+    const canvas = document.getElementById('graphCanvas'); // Assuming you have a canvas with this ID
+    const ctx = canvas.getContext('2d');
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);  // Clear the canvas
+
+    Object.keys(primaryNodes).forEach(lineNum => {
+        primaryNodes[lineNum].forEach(node => {
+            drawNode(ctx, node.x, node.y);
+        });
+
+        // Optionally, you can draw lines between the nodes
+        for (let i = 0; i < primaryNodes[lineNum].length - 1; i++) {
+            drawLine(ctx, primaryNodes[lineNum][i], primaryNodes[lineNum][i + 1]);
+        }
+    });
+}
+
+// Function to draw a node on the canvas
+function drawNode(ctx, x, y) {
+    ctx.beginPath();
+    ctx.arc(x, y, 3, 0, Math.PI * 2, true);  // Small circle to represent the node
+    ctx.fillStyle = 'blue';
+    ctx.fill();
+}
+
+// Function to draw a line between two nodes
+function drawLine(ctx, node1, node2) {
+    ctx.beginPath();
+    ctx.moveTo(node1.x, node1.y);
+    ctx.lineTo(node2.x, node2.y);
+    ctx.strokeStyle = 'blue';
+    ctx.stroke();
+}
