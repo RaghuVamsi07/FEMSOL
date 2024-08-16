b8895c4c40cebe781c7be8940cee6608304e3d31
RaghuVamsi07 <155610623+RaghuVamsi07@users.noreply.github.com>
2024-08-16 16:48:53 +0530

    Update validation.js

--- GLML1DS/1D_Bars/static/js/validation.js
+++ GLML1DS/1D_Bars/static/js/validation.js
@@ -1,24 +1,4 @@
-let selectedElementType = null;
-
-// Function to set the selected element type when a button is clicked
-function selectElementType(type) {
-    selectedElementType = type;
-}
-
-// Function to check if an element type is selected
-function checkElementTypeSelected() {
-    return selectedElementType !== null;
-}
-
-// Main validation function called on button click
 function validateAndProceed() {
-    let elementTypeSelected = checkElementTypeSelected();
-
-    if (!elementTypeSelected) {
-        if (confirm("No element type (linear or quadratic) is selected. This may produce wrong results. Do you want to proceed?")) {
-            window.open('/results', '_blank');  // Open results page in a new tab
-        }
-    } else {
-        window.open('/results', '_blank');  // Open results page in a new tab
-    }
+    // Simply open the results page in a new tab without any conditions
+    window.open('/results', '_blank');  // Open results page in a new tab
 }
